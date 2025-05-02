2e8c143ff5eed460763405cd89cdde0a1f428e9c
Proyecto-maquipan <crispimentelm@gmail.com>
2025-05-01 20:31:54 -0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  databaseURL: "https://TU_PROYECTO.firebaseio.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
